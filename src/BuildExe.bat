46f0677af91ff94059fb6f3553af60cd96e3da29
varungoyal <varungoyal1994@gmail.com>
2018-02-11 00:21:16 +0530

    -Added support for Icon file in exe -Added ReadMe.txt

--- src/BuildExe.bat
+++ src/BuildExe.bat
@@ -1,2 +1,6 @@
-C:\Python27\Scripts\pyinstaller.exe -F Starter.py 
+@echo off
+#C:\Python27\Scripts\pyi-grab_version.exe Starter version.rc   
+python -O -m PyInstaller -F Starter.py --icon=ico.ico
+REM C:\Python27\Scripts\pyinstaller.exe -F Starter.py  
+REM --add-data="ico.ico;ico" --icon=ico.ico
 set /p DUMMY=Hit ENTER to continue...
---
+++ src/ReadMe.txt
@@ -0,0 +1,5 @@
+This is a sample project showing how to create exe using pyinstaller.
+You will need pyinstaller and then you need to run the BuildExe.bat
+Note:
+1. To add icon, ico file should be in same folder as of folder in which binary is generated.
+2. Need to install pyinstaller and need to set path accordingly.
